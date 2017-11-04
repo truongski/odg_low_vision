47fa80c97984efaabed5d83c57e7c42250cfc928
Humam Nameer <humamnameer@Ennovar-MarCom13-4.local>
2017-11-04 15:21:50 -0500

    controls

--- Assets/Core/MouseMoveable.cs
+++ Assets/Core/MouseMoveable.cs
@@ -13,6 +13,10 @@ namespace Core
 
         public float doubleClickDelay = 0.5f;
 
+		public float mouseSensitivity = 1.0f;
+		private Vector3 lastPosition;
+
+
         private void Start()
         {
             originalPosition = this.transform.localPosition;
@@ -20,31 +24,44 @@ namespace Core
         }
         private void Update()
         {
-            var currentPosition = Input.mousePosition;
-            Vector3 deltaPosition = currentPosition - previousPosition;
+//            var currentPosition = Input.mousePosition;
+//            Vector3 deltaPosition = currentPosition - previousPosition;
+//
+//            if (Input.GetMouseButton(0))
+//            {
+//                Vector3 forward = this.transform.parent.transform.forward * deltaPosition.y * sensitivity;
+//                this.transform.position += forward;
+//                pressed = true;
+//            }
+//            else
+//            {
+//                if (pressed)
+//                {
+//                    if (Time.time < lastClick + doubleClickDelay)
+//                    {
+//                        this.transform.localPosition = this.originalPosition;
+//                    }
+//
+//                    lastClick = Time.time;
+//                    pressed = false;
+//                }
+//            }
+//
+//            previousPosition = currentPosition;
 
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 forward = this.transform.parent.transform.forward * deltaPosition.y * sensitivity;
-                this.transform.position += forward;
 
-                pressed = true;
-            }
-            else
-            {
-                if (pressed)
-                {
-                    if (Time.time < lastClick + doubleClickDelay)
-                    {
-                        this.transform.localPosition = this.originalPosition;
-                    }
 
-                    lastClick = Time.time;
-                    pressed = false;
-                }
-            }
+			if (Input.GetMouseButtonDown(0))
+			{
+				lastPosition = Input.mousePosition;
+			}
 
-            previousPosition = currentPosition;
+			if (Input.GetMouseButton(0))
+			{
+				Vector3 delta = Input.mousePosition - lastPosition;
+				transform.Translate(delta.x * mouseSensitivity, delta.y * mouseSensitivity, 0);
+				lastPosition = Input.mousePosition;
+			}
         }
     }
 }
---
+++ Assets/UI_Interactions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Interactions : MonoBehaviour {
+
+	public float sensitivity = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void zoomIn(){
+		Vector3 forward = this.transform.parent.transform.forward + new Vector3(0, 0, 2);
+		this.transform.position += forward;
+	}
+
+	public void zoomOut(){
+		Vector3 forward = this.transform.parent.transform.forward + new Vector3(0, 0, -2);
+		this.transform.position += forward;
+	}
+
+	public void NavigateX(float x){
+		Vector3 forward = this.transform.forward + new Vector3(x,0,0);
+		this.transform.position += forward;
+	}
+	public void NavigateY(float y){
+		Vector3 forward = this.transform.forward + new Vector3(0,y,0);
+		this.transform.position += forward;
+	}
+}

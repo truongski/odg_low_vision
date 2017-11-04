9c60b5f9be51db6ed51929b6b4ffdf96cbd90f38
Humam Nameer <humamnameer@Ennovar-MarCom13-4.local>
2017-11-04 16:02:47 -0500

    feature/UI

--- Assets/Core/WebCamBehaviour.cs
+++ Assets/Core/WebCamBehaviour.cs
@@ -8,13 +8,33 @@ namespace Core
         private WebCamTexture texture;
         [SerializeField]
         private Material targetMaterial;
+		private Texture2D tex; 
+		Color[] colors = new Color[900];
+
         private void Start()
         {
+			tex = new Texture2D(256, 256);
             texture = new WebCamTexture();
-            
+
             texture.Play();
 
-            targetMaterial.mainTexture = texture;
+
+//
+//			colors = texture.GetPixels(100, 100, 200,200);
+//			tex.SetPixels (0, 0, 200, 200, colors);
+//			tex.Apply();
+//
+//			targetMaterial.mainTexture = tex;
+
         }
+
+		void FixedUpdate () {
+			
+			colors = texture.GetPixels(100, 100, 200,200);
+			tex.SetPixels (0, 0, 200, 200, colors);
+			tex.Apply();
+
+			targetMaterial.mainTexture = tex;
+		}
     }
 }
